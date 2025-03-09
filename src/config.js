d488c678ec52e3144fa0dbd0f7f75bfaa710b17c
krisp1 <mrkph01@gmail.com>
2025-03-08 23:16:35 -0500

    github username src/config.js

--- src/config.js
+++ src/config.js
@@ -35,7 +35,7 @@ export { HeroDark as Dark };
   Add a second paragraph for the about me section.
 */
 export const moreInfo =
-  "I enjoy learning about technology and helping others use it to improve their lives and be more productive. I built this site with React, React Bootstrap, Redux, and the GitHub REST API.";
+  "I enjoy learning about technology and helping others use it to improve their lives and be more productive. I have recently completed Cert IV - Cyber Security at Melbourne Polytechnic."
 
 /* Skills
  ************************************************************** 
